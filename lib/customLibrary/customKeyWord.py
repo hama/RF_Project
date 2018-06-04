04f667e722f094964c59e22e986c13cff1a004b6
lizhicheng1997 <1004714019@qq.com>
2018-06-04 15:39:26 +0800

    add case;

--- lib/customLibrary/customKeyWord.py
+++ lib/customLibrary/customKeyWord.py
@@ -215,3 +215,29 @@ class keyWord(object):
             return -1
 
         return res_list[arg]
+
+    def selectProductCountByStatus(self, arg):
+        import requests
+
+        if arg:
+            arg = int(arg)
+        else:
+            return 0
+
+        x_url = "http://admin1024.shoplazza.com/api/user/login"
+        p_url = "http://admin1024.shoplazza.com/api/product/search?status=" + str(arg) + "&page=0&limit=20"
+        datas = {"contact": "18825260804", "password": "18825260804", "username": "diu"}
+        res = requests.post(url=x_url,headers={},data=datas)
+        if res is None or res.status_code != 200:
+            return res.status_code
+        cookiesx = '; '.join(['='.join(item) for item in res.cookies.items()])
+
+        uid = json.loads(res.content)['data']['id'] or None
+
+        sub_list = requests.get(url=p_url,headers={"cookie": cookiesx})
+        res_data = json.loads(sub_list.content)['data']['products']
+        res_list = []
+        for i in res_data:
+            res_list.append(i['title'])
+
+        return len(res_list)
