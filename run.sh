71f705f57dafe4fa6faf0be470aff88a2550a088
junchenchen <171869092@qq.com>
2018-07-25 15:47:47 +0800

    run脚本修改

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python lib/customLibrary/customKeyWord.py --url=http://admin1024.shoplazza.com
-robot -v is_headless:True  module/
+python2.7 lib/customLibrary/customKeyWord.py --url=http://admin1024.shoplazza.com
+robot -v is_headless:True -l /01order  module/01_order
+robot -v is_headless:True -l /02product  module/02_product
+robot -v is_headless:True -l /06shipping  module/08_settings/06_shipping
+robot -v is_headless:True -l /09checkout  module/09_checkout
