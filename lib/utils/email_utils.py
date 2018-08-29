6cd3d9e49a0b0a744901f07e7b26d3694e1196a6
Homer <homerchien0224@gmail.com>
2018-08-29 20:14:32 +0800

    for online step8

--- lib/utils/email_utils.py
+++ lib/utils/email_utils.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
+import ConfigParser
 import email
 import os
 import smtplib
+import sys
 import time
 from email.header import Header
 from email.mime.image import MIMEImage
@@ -9,27 +11,26 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 username = "autotest@shoplazza.com"  # .发件人
 password = "Robot@123"  # .发件人密码
 relative_path = os.path.join(os.path.dirname(__file__), '../..')
 # . 收件邮箱
 to_addr = [
-    # 'wanglinyun@shoplazza.com',
-    # 'xiabing@shoplazza.com',
-    # 'qiansuixin@shoplazza.com',
+    'wanglinyun@shoplazza.com',
+    'xiabing@shoplazza.com',
+    'qiansuixin@shoplazza.com',
     'lijunchen@shoplazza.com'
 ]
 # . 抄送邮箱
 cc_addr = [
-    # 'zhangliqiong@shoplazza.com',
-    # 'yupeng@shoplazza.com',
-    # 'linyingjun@shoplazza.com',
-    # 'fangbaiyuan@shoplazza.com'
+    'zhangliqiong@shoplazza.com',
+    'yupeng@shoplazza.com',
+    'linyingjun@shoplazza.com',
+    'fangbaiyuan@shoplazza.com'
 ]
-email_service = "smtp.alibaba.com"
-default_port = 25
+email_service = "smtp.mxhichina.com"
+default_port = 465
 
 
 def send_email():
@@ -39,7 +40,7 @@ def send_email():
     '''
     msg = email_fomat_content()
     try:
-        smtp = smtplib.SMTP()
+        smtp = smtplib.SMTP_SSL()
         smtp.connect(email_service, default_port)
         smtp.set_debuglevel(0)
         smtp.login(username, password)
@@ -67,8 +68,10 @@ def get_screenshot():
     报告截屏
     :return:
     '''
-    chrome_options = Options()
+    chrome_options = sys.modules['selenium.webdriver'].ChromeOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('no-sandbox')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.set_window_size(1200, 900)
     file_path = 'file:///' + os.getcwd() + '/logs/report.html'
@@ -83,8 +86,14 @@ def email_fomat_content():
     邮件格式&内容
     :return:
     '''
+    # 获取测试环境信息
+    config = ConfigParser.ConfigParser()
+    path = os.path.join(os.path.dirname(__file__), '../..') + '/config/common.ini'
+    config.read(path)
+    env_detail = config.get("common_url", "home_page_url")
+
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = Header('Robot测试报告'.decode('utf-8')).encode()
+    msg['Subject'] = '%s (环境：%s)' % (Header('Robot测试报告'.decode('utf-8')).encode(), env_detail)
     msg['From'] = '%s <%s>' % (Header('自动化测试'.decode('utf-8')).encode(), username)
     msg['To'] = ', '.join(to_addr)
     msg['Cc'] = ', '.join(cc_addr)
@@ -99,11 +108,13 @@ def email_fomat_content():
     msgText = MIMEText(html, 'html', 'utf-8')
     msg.attach(msgText)
     # 图片
+
     fp = open(relative_path + '/logs/screenshot_for_report.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', '<image1>')
     msg.attach(msgImage)
+
     # 附件
     att = MIMEText(open(relative_path + '/logs/log.html', 'rb').read(), 'base64', 'utf-8')
     att["Content-Type"] = 'application/octet-stream'
@@ -115,5 +126,5 @@ def email_fomat_content():
 
 if __name__ == "__main__":
     get_screenshot()
-    # time.sleep(5)
-    # send_email()
+    time.sleep(5)
+    send_email()
