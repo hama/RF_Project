8a870675b94d70deba9c0ba1a36f3f855b6c5032
Homer <homerchien0224@gmail.com>
2018-08-30 16:05:05 +0800

    for online step9

--- run.sh
+++ run.sh
@@ -1,14 +1,79 @@
 #!/bin/bash
-if [ $# -eq 1 ]
+
+export PATH=$PATH:/usr/local/bin/
+#定义选项， -o 表示短选项 -a 表示支持长选项的简单模式(以 - 开头) -l 表示长选项
+# a 后没有冒号，表示没有参数
+# b 后跟一个冒号，表示有一个必要参数
+# c 后跟两个冒号，表示有一个可选参数(可选参数必须紧贴选项)
+# -n 出错时的信息
+# -- 也是一个选项，比如 要创建一个名字为 -f 的目录，会使用 mkdir -- -f ,
+#    在这里用做表示最后一个选项(用以判定 while 的结束)
+# $@ 从命令行取出参数列表(不能用用 $* 代替，因为 $* 将所有的参数解释成一个字符串
+#                         而 $@ 是一个参数数组)
+
+TEMP=`getopt -o ec:u: -l email,exec:,url: -- "$@"`
+
+# 判定 getopt 的执行时候有错，错误信息输出到 STDERR
+if [ $? != 0 ]
 then
-    python2.7 lib/customLibrary/customKeyWord.py --url=$1
-elif [ $# -eq 0 ]
-then
-    python2.7 lib/customLibrary/customKeyWord.py
-else
-    echo 'the num of params is wrong!'
+	echo "Terminating....." >&2
+	exit 1
 fi
 
-robot -v is_headless:True -d logs/  module/03_login/*
+# 重新排列参数的顺序
+# 使用eval 的目的是为了防止参数中有shell命令，被错误的扩展。
+eval set -- "$TEMP"
 
-python2.7 lib/utils/email_utils.py
+# 处理具体的选项
+while true
+do
+	case "$1" in
+		-e | --email)
+			send_email = 'y'
+			shift
+			;;
+		-c | --exec)
+			test_exec =  "$2"
+			shift 2
+			;;
+		-u | --url)
+			test_url =  "$2"
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Internal error!"
+			exit 1
+			;;
+	esac
+done
+
+# 执行customKeyWord.py
+if [ "$test_url" ]
+then
+    python2.7 lib/customLibrary/customKeyWord.py --url="$test_url"
+else
+    python2.7 lib/customLibrary/customKeyWord.py
+fi
+# 执行用例
+if [ "$test_exec" ]
+then
+    robot -v is_headless:True -d logs/ "$test_exec"
+else
+    robot -v is_headless:True -d logs/ module/03_login/*
+fi
+# 执行email_utils.py
+if [ "$send_email" ]
+then
+    python2.7 lib/utils/email_utils.py
+fi
+
+
+#显示除选项外的参数(不包含选项的参数都会排到最后)
+# arg 是 getopt 内置的变量 , 里面的值，就是处理过之后的 $@(命令行传入的参数)
+#for arg do
+#   echo '--> '"$arg" ;
+#done
