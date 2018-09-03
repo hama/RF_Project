c2558b4109dc69bbaebc45cdf652544411665569
Homer <homerchien0224@gmail.com>
2018-09-03 20:44:49 +0800

    for ci step20

--- lib/customLibrary/customKeyWord.py
+++ lib/customLibrary/customKeyWord.py
@@ -742,10 +742,10 @@ if __name__ == '__main__':
         config.set("common_db", "db_service_config", '{"host": "rm-wz9tz4ff2v9t95f9qao.mysql.rds.aliyuncs.com",'
                                                      '"port": 3306,"user": "app","password": "8rEdunuH@Fe+",'
                                                      '"db": "service","charset": "utf8"}')
+        config.set("common_url", "home_page_url", args.url)
         # 测试服，使用新注册用户跑用例
         if args.user == 'new':
             random_str = keyWord().salt()
-            config.set("common_url", "home_page_url", args.url)
             config.set("common_account", "datas_contact", random_str + "@abctest.com")
             config.set("common_account", "datas_domain", random_str)
             config.write(open(path, 'w'))
@@ -754,3 +754,5 @@ if __name__ == '__main__':
             kw.sign_up()
             # 注册用户之后等待10s，等用户生效
             time.sleep(10)
+        else:
+            config.write(open(path, 'w'))
