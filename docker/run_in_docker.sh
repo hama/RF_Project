dae0ff3f70a6377f3343bd6fc74254a4ea317b80
Homer <homerchien0224@gmail.com>
2018-09-13 20:20:48 +0800

    docker build

--- docker/run_in_docker.sh
+++ docker/run_in_docker.sh
@@ -1,1 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+docker run -it myimage \
+cd /opt && \
+git clone git@gitlab.sealmoo.com:shoplaza/shoplaza_robot.git && \
+cd /opt/shoplaza_robot/ && \
+bash run.sh -u https://admin.shoplazza.com -e > /tmp/run_robot.log 2>&1
