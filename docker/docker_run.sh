5da32913227b2d4a3a79f90bb602a18acd9a4fc5
Homer <homerchien0224@gmail.com>
2018-10-20 16:55:54 +0800

    a

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo `date` >> ~/cron.log
-docker run -i -v /dev/shm:/dev/shm --rm registry.shoplazza.com/library/uitest:v5 bash -c "/opt/run_in_docker.sh -e"
+docker run -i -v /dev/shm:/dev/shm --rm registry.shoplazza.com/library/uitest:v5 bash -c "/opt/run_in_docker.sh -u https://admin.shoplazza.com -e"
