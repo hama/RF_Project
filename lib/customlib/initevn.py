a397fa9a8d5990a455e68660d82bf979e0bf6039
Homer <homerchien0224@gmail.com>
2018-11-07 09:19:04 +0800

    f

--- lib/customlib/initevn.py
+++ lib/customlib/initevn.py
@@ -30,11 +30,11 @@ if __name__ == '__main__':
     #            '{"host": "rm-rj9f22zpw641v45g9.mysql.rds.aliyuncs.com","port": 3306,"user": "lansejiebo",'
     #            '"password": "lansejiebo@123","db": "shop_","charset": "utf8"}')
 
-    # if 'https://admin.shoplazza.com' in args.url:
-    #     config.set("common_db", "db_service_config",
-    #                '{"host": "rm-rj978v5t5e6462r66.mysql.rds.aliyuncs.com","port": 3306,'
-    #                '"user": "readonly","password": "readonly@123","db": "service","charset": "utf8"}')
-    #     config.set("common_urn", "shop_urn", '.myshoplaza.com')
+    if 'https://admin.shoplazza.com' in args.url:
+        config.set("common_db", "db_service_config",
+                   '{"host": "rm-rj978v5t5e6462r66.mysql.rds.aliyuncs.com","port": 3306,'
+                   '"user": "readonly","password": "readonly@123","db": "service","charset": "utf8"}')
+        config.set("common_urn", "shop_urn", '.myshoplaza.com')
     if 'new' in args.user:
         # 使用新注册用户跑用例
         random_str = salt_py()
@@ -53,6 +53,6 @@ if __name__ == '__main__':
 
     # --------------------配置国内B端映射 host 记录 47.106.30.167 admin.shoplazza.com--------------------
     # --------------------配置国内C端映射 host 记录 47.106.30.164 random_str.myshoplaza.com--------------------
-    os.popen('echo "47.106.30.167 admin.shoplazza.com" >> /etc/hosts')
-    os.popen('echo "47.106.30.164 %s.myshoplaza.com" >> /etc/hosts' % (random_str))
+    # os.popen('echo "47.106.30.167 admin.shoplazza.com" >> /etc/hosts')
+    # os.popen('echo "47.106.30.164 %s.myshoplaza.com" >> /etc/hosts' % (random_str))
     print "--------------------initevn.py has done--------------------"
