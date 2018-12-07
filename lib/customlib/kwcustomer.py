8d3f0c5e2e914ac78c1080316325103225721640
Homer <homerchien0224@gmail.com>
2018-12-07 14:23:18 +0800

    add customer api

--- lib/customlib/kwcustomer.py
+++ lib/customlib/kwcustomer.py
@@ -7,11 +7,161 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def admin_customers_address_settings_py(data, cookie=init_cookie):
+def customers_get_py(query_str={}, cookie=init_cookie):
     '''
-
+    用户列表
+    :param query_str:
     :param cookie:
     :return:
     '''
-    url = home_page_url + "/api/admin/customers/address_settings"
+
+    url = '%s/api/admin/customers' % (home_page_url)
+    return do_get(url, query_str, cookie=cookie)
+
+
+def customers_export_py(query_str={}, cookie=init_cookie):
+    '''
+    导出用户
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+
+    url = '%s/api/admin/customers/export.json' % (home_page_url)
+    return do_get(url, query_str, cookie=cookie)
+
+
+def customers_id_get_py(customer_id, cookie=init_cookie):
+    '''
+    用户详情
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+    url = '%s/api/admin/customers/%s' % (home_page_url, customer_id)
+    return do_get(url, {}, cookie=cookie)
+
+
+def customers_post_py(data, cookie=init_cookie):
+    '''
+    创建用户
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+    url = home_page_url + '/api/admin/customers'
+    return do_post(url, data, cookie=cookie)
+
+
+def customers_patch_py(data, customer_id, cookie=init_cookie):
+    '''
+    修改用户
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+    url = '%s/api/admin/customers/%s' % (home_page_url, customer_id)
     return do_patch(url, data, cookie=cookie)
+
+
+def customers_delete_py(customer_id, cookie=init_cookie):
+    '''
+    删除用户
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+    url = '%s/api/admin/customers/%s' % (home_page_url, customer_id)
+    return do_delete(url, cookie=cookie)
+
+
+def customers_id_addresses_py(customer_id, cookie=init_cookie):
+    '''
+    用户地址列表
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+
+    url = '%s/api/admin/customers/%s/addresses' % (home_page_url, customer_id)
+    return do_get(url, {}, cookie=cookie)
+
+
+def customers_id_addresses_id_py(customer_id, addresse_id, cookie=init_cookie):
+    '''
+    用户地址详情
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+
+    url = '%s/api/admin/customers/%s/addresses/%s' % (home_page_url, customer_id, addresse_id)
+    return do_get(url, {}, cookie=cookie)
+
+
+def customers_import_post_py(data, cookie=init_cookie):
+    '''
+    导入用户
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+    url = home_page_url + '/api/admin/customer_imports'
+    return do_post(url, data, cookie=cookie)
+
+
+def customers_import_id_py(import_id, cookie=init_cookie):
+    '''
+    查询导入状态
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+
+    url = '%s/api/admin/customer_imports/%s' % (home_page_url, import_id)
+    return do_get(url, {}, cookie=cookie)
+
+
+def customers_import_get_py(query_str={}, cookie=init_cookie):
+    '''
+    查询导入列表
+    :param query_str:
+    :param cookie:
+    :return:
+    '''
+
+    url = '%s/api/admin/customer_imports' % (home_page_url)
+    return do_get(url, query_str, cookie=cookie)
+
+
+def del_all_customers_py(cookie=init_cookie):
+    """
+    删除全部顾客
+    :return: True | False
+    """
+    customer_ids = get_exist_customersid_py(cookie=cookie)
+    for customer_id in customer_ids:
+        customers_delete_py(customer_id, cookie=cookie)
+
+
+def get_exist_customersid_py(cookie=init_cookie):
+    '''
+    返回存在的所有顾客id
+    :param cookie:
+    :return:
+    '''
+    count = get_all_customers_count_py(cookie=cookie)
+    customers_list = customers_get_py({'page': 1, 'per_page': count}, cookie=cookie)['content']['customers']
+    customers_id = []
+    for customer in customers_list:
+        customers_id.append(customer['id'])
+    return customers_id
+
+
+def get_all_customers_count_py(cookie=init_cookie):
+    return customers_get_py(cookie=cookie)['content']['count']
+
+
+if __name__ == '__main__':
+    print del_all_customers_py()
+    print customers_get_py()
